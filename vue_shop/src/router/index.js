6bb67c87b12ce037fe2b4ab237469416d8c2ca73
czy <Prog@git.com>
2020-06-18 08:57:29 +0800

    software addin

---
+++ vue_shop/src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path:'/login',
+    component:Login
+  },
+  {
+    path:'/',
+    redirect:'/login'
+  },
+  {
+    path:'/home',
+    component:Home
+  }
+  
+]
+
+const router = new VueRouter({
+  routes
+});
+
+//挂载路由导航守卫
+router.beforeEach((to,from,next) => {
+  // to 将要访问的路径
+  // from 代表产品卖给哪个路径来
+  // next 是一个函数，用于放行或跳转
+  // next() 放行；；；next('/login') 强制跳转登录
+  if(to.path === '/login') return next();
+  const tokenStr = window.sessionStorage.getItem("token");
+  if(!tokenStr){return next('/login');}
+  next();
+});
+export default router
